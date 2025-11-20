2da656c403d3e76b0a62ea467139f6936e5c5c0b
Shreyas <ss4shreyas@gmail.com>
2025-11-20 16:34:30 +0100

    parrale edit

--- parallel.py
+++ parallel.py
@@ -7,9 +7,9 @@ def run_resource(resource_name: str):
     source = github_pipeline.github_source.with_resources(resource_name)
 
     pipeline = dlt.pipeline(
-        pipeline_name=f"github_remote_demo_{resource_name}",
+        pipeline_name=f"github_remote_orc_test_demo_{resource_name}",
         destination="bigquery",
-        dataset_name="demo_remote_github",
+        dataset_name="demo_remote_orc_test_github",
         progress="log",
     )
 
